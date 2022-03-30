5e4ba65e4588e03916802338034b852ef3b8f20b
Giovanna Rosa <gmarcaro@gmail.com>
2022-03-30 17:37:49 -0300

    api

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from '../../sevices/api'
 import dividerImgDesk from '../../assets/pattern-divider-desktop.svg'
 import dividerImgMob from '../../assets/pattern-divider-mobile.svg'
 import diceImg from '../../assets/icon-dice.svg'
@@ -6,15 +7,25 @@ import diceImg from '../../assets/icon-dice.svg'
 import './styles.scss'
 
 export function Card(){
+  const [advice, setAdvice] = useState()
+
+  async function getAdvice() {
+    const response = await api.get('/advice')
+
+    setAdvice(response.data.slip)
+  }
+
+  useEffect(() => {
+    getAdvice()
+  }, [])  
+
   return (
     <div className="container">
       <div className="card-box">
-        <h1 className="title">ADVICE &#x00023; 117</h1>
-        <p className="quote">
-          "It is easy to sit up and take notice, what's difficult is getting up and taking action."
-        </p>
-        <img src={window.screen.width > 768 ? dividerImgDesk : dividerImgMob} alt="Divider" />
-        <button className="dice">
+        <h1 className="title">ADVICE &#x00023; {advice?.id}</h1>
+        <p className="quote">{advice?.advice}</p>
+        <img src={window.screen.width > 768 ? dividerImgDesk : dividerImgMob} alt="Divider" className="divider" />
+        <button onClick={getAdvice} className="dice">
           <img src={diceImg} alt="Dice icon" />
         </button>
       </div>
